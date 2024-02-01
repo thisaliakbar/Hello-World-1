be869e6aa8d9bf382b3abb0235d2ec044a8f5f0b
E31230784_MOH ALI AKBAR RAFSANJANI <mohaliakbarrafsanjani@gmail.com>
2024-02-01 20:33:21 +0700

    Mengubah Teks menjadi World War III

--- hello-world-1/src/main/Test.java
+++ hello-world-1/src/main/Test.java
@@ -11,6 +11,6 @@ package main;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello Dunia");
+        System.out.println("World War III");
     }
 }
